80d8179f486edf1ba9a1ee260314b4b537f49f54
James <contact@jamesj.me>
2017-03-26 23:09:11 +0100

    Initial commit

---
+++ src/main/java/me/jamesj/bungeestaffchat/BungeeStaffChat.java
@@ -0,0 +1,16 @@
+package me.jamesj.bungeestaffchat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class BungeeStaffChat extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
